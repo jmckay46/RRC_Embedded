c0dfa8d8a75770f7b142f13cb4b2aac0e5443c6a
jmckay46 <jmckay586@gmail.com>
2018-10-10 02:28:32 +0000

    new mux architecture

--- Digital_Systems/lab2/quad2to1mux.vhd
+++ Digital_Systems/lab2/quad2to1mux.vhd
@@ -1,16 +1,17 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity quad4to1mux is
+entity quad2to1mux is
 port (
 		a		:in	std_logic_vector(3 downto 0);
 		b		:in	std_logic_vector(3 downto 0);
 		s		:in	std_logic;
 
 		y		:out	std_logic_vector(3 downto 0));
-end quad4to1mux;
+end quad2to1mux;
 
-architecture circuit of quad4to1mux is
+architecture mux of quad2to1mux is
 begin
-		y <= a when (s = '1') else b;
-end circuit;
+	with s select
+		y <= a when '0', b when '1', "0000" when others;
+end mux;
