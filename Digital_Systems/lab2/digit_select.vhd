750bada5b386db7108ebe4867f5b8718c9dd16e0
jmckay46 <jmckay586@gmail.com>
2018-10-10 02:31:15 +0000

    added component instantiation statement

--- Digital_Systems/lab2/digit_select.vhd
+++ Digital_Systems/lab2/digit_select.vhd
@@ -2,41 +2,46 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity digit_select is
-    port(
-            a_in        : in    std_logic_vector (3 downto 0);
-            b_in        : in    std_logic_vector (3 downto 0);
-            sel         : in    std_logic_vector;
-            display     : out   std_logic_vector (0 to 6));
-
+port(
+		a_in 		: in std_logic_vector (3 downto 0);
+		b_in 		: in std_logic_vector (3 downto 0);
+		sel 		: in std_logic;
+		
+		display 	: out std_logic_vector (0 to 6));		
 end digit_select;
 
 architecture mux of digit_select is
 
+--Component declaration is the same as the entity declaration in quad2to1mux
 component quad2to1mux
-    port(
-            a       : in    std_logic_vector (3 downto 0);
-            b       : in    std_logic_vector (3 downto 0);
-            s       : in    std_logic_vector;
-            y       : out   std_logic_vector (3 downto 0));
+port(
+		a 		: in std_logic_vector (3 downto 0);
+		b 		: in std_logic_vector (3 downto 0);
+		s 		: in std_logic;
+		
+		y 		: out std_logic_vector (3 downto 0));
 end component;
 
+-- Component declaration is the same as the entity declaration n hex7seg
 component hex7seg
-    port(
-            d       : in    std_logic_vector (3 downto 0);
-            y       : out   std_logic_vector (0 to 6));
+port(
+		d 		: in std_logic_vector(3 downto 0);
+		
+		y 		: out std_logic_vector(0 to 6));
 end component;
 
 --declare a signal to connect the two components
-
-signal selected_data    :   std_logic_vector (3 downto 0);
+signal selected_data : std_logic_vector(3 downto 0);
 begin
---concatenate the output from quad2to1mux into selected_data
-    selected_data <= Y3 & Y2 & Y1 & Y0;
-
---separate each port into inputs of hex7seg
-    d(3) <= selected_data(3);
-    d(2) <= selected_data(2);
-    d(1) <= selected_data(1);
-    d(0) <= selected_data(0);
-
+--explicit component instantiation 
+multiplexer: quad2to1mux
+port map(	s	=>		sel,
+		a	=>		a_in,
+		b	=>		b_in,
+		y	=>		selected_data);
+				
+decoder: hex7seg
+port map(	d	=>		selected_data,
+		y	=>		display);
+				
 end mux;
