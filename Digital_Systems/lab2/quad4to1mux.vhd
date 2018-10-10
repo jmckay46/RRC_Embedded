877212974435fcffab11a574b17241e15e12750c
jmckay46 <jmckay586@gmail.com>
2018-10-10 01:24:45 +0000

    init_1

--- Digital_Systems/lab2/quad4to1mux.vhd
+++ Digital_Systems/lab2/quad4to1mux.vhd
@@ -1,16 +1,16 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity quad2to1mux is
+entity quad4to1mux is
 port (
 		a		:in	std_logic_vector(3 downto 0);
 		b		:in	std_logic_vector(3 downto 0);
 		s		:in	std_logic;
 
 		y		:out	std_logic_vector(3 downto 0));
-end quad2to1mux;
+end quad4to1mux;
 
-architecture circuit of quad2to1mux is
+architecture circuit of quad4to1mux is
 begin
 		y <= a when (s = '1') else b;
 end circuit;
